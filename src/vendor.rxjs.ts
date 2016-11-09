a28979f43bf6eb76526d41512c2944043e65e29b
Fabian Mueller <fmueller@Fabians-MBP-4.fritz.box>
2016-11-09 22:43:01 +0100

    Disable unused RxJS vendor imports

--- src/vendor.rxjs.ts
+++ src/vendor.rxjs.ts
@@ -1,14 +1,7 @@
 // Observables
 // import 'rxjs/add/Observable/of';
-// import 'rxjs/add/Observable/timer';
-import 'rxjs/add/Observable/throw';
+// import 'rxjs/add/Observable/throw';
 
 // Operators
-import 'rxjs/add/operator/catch';
-// import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/publish';
-// import 'rxjs/add/operator/share';
-// import 'rxjs/add/operator/startWith';
+// import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/switchMap';
